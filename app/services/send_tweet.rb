5d93c9f8902f3b34a3c64f40136c197992202117
Romain Mosser <mosser.romain@gmail.com>
2017-11-09 18:29:23 +0100

    modification du fichier send_tweet.rb

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -1,24 +1,21 @@
+require 'twitter'
 class SendTweet
-
-  attr_reader :tweet
   def initialize(tweet)
     #fait passer le string en paramètre
     @tweet = tweet
   end
-
-  def log_in_to_twitter
+  def log_in_twitter
     #log in avec credentials
-    client = Twitter::Streaming::Client.new do |config|
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "eIWRgUIPCJVnNIDI05ZIX3963"
       config.consumer_secret     = "cHa0UVLoeZV9boot3NVGSvmHRt1mWz02V7ADJBnZ0bYrd4bldS"
       config.access_token        = "918050050529521665-Bp6rxOPbgm3fQj6mxZTbGUtUx6R9ksF"
       config.access_token_secret = "uMgveGD9dU3RSATAFsd0aqOVNa5nnVExh3cENoVJPk0W9"
     end
   end
-
-  def send_tweet(tweet)
+  def perform
     #envoies le tweet
-    client.update(tweet)
+    log_in_twitter
+    @client.update(@tweet)
   end
-
 end
